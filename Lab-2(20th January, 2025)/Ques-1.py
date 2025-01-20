9fea34e41945b0134ba2d55e0930f78f3850bbcb
Aanidhay Aggarwal <aanidhayaggarrwal2004@gmail.com>
2025-01-20 11:47:38 +0530

    added folder and code in the files of the Lab 2 Experiments

---
+++ Lab-2(20th January, 2025)/Ques-1.py
@@ -0,0 +1,7 @@
+#Create and access tuples
+
+colors = ("red", "blue", "green", "yellow", "blue")
+print("First color:", colors[0])    
+print("Last color:", colors[-1])
+blue_count = colors.count("blue")
+print("Number of times 'blue' appears:", blue_count)
---
+++ Lab-2(20th January, 2025)/Ques-4.py
@@ -0,0 +1,6 @@
+#Create a program to print the multiplication table of a number.
+
+number = int(input("Enter a number to print its multiplication table: "))
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} x {i} = {product}")
